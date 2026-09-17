2f60ad1be8ce32ce501e492641e7f803a2786423
Isis Krystal Agramón Leal <a00843802@tec.mx>
2026-09-17 12:40:53 -0600

    Evidencia 1. Parte 3

--- Evidences/Evidence1/Log.h
+++ Evidences/Evidence1/Log.h
@@ -48,3 +48,62 @@ bool Log::operator>=(const Log&log1) const{
 bool Log::operator<=(const Log&log1) const {
     return key <= log1.key;
 }
+
+string Log::createKey() {
+    string monthNumber;
+    string dayNumber;
+if (month == "Jan"){
+    monthNumber = "01";
+}
+
+else if (month == "Feb"){
+    monthNumber = "02";
+}
+
+else if (month == "Mar"){
+    monthNumber = "03";
+}
+
+else if (month == "Apr"){
+    monthNumber = "04";
+}
+
+else if (month == "May"){
+    monthNumber = "05";
+}
+
+else if (month == "Jun"){
+    monthNumber = "06";
+}
+
+else if (month == "Jul"){
+    monthNumber = "07";
+}
+
+else if (month == "Aug"){
+    monthNumber = "08";
+}
+
+else if (month == "Sep"){
+    monthNumber = "09";
+}
+
+else if (month == "Oct"){
+    monthNumber = "10";
+}
+
+else if (month == "Nov"){
+    monthNumber = "11";
+}
+
+else if (month == "Dec"){
+    monthNumber = "12";
+}
+
+else{
+    return "";
+}
+}
+
+
+
