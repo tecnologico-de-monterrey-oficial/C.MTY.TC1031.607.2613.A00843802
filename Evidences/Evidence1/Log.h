43bef1e657bff986b9355289d97556af50630f89
Isis Krystal Agramón Leal <a00843802@tec.mx>
2026-09-17 12:22:16 -0600

    Evidencia 1. Parte 2

--- Evidences/Evidence1/Log.h
+++ Evidences/Evidence1/Log.h
@@ -8,11 +8,13 @@ struct Log{
     int year;
     string month;
     int day;
+    string time;
     string ip;
     string message;
     string key;
+    
     Log();
-    Log(int year, string month, int day, string ip, string message, string key);
+    Log(int year, string month, int day, string time, string ip, string message, string key);
     string createKey();
     bool operator>(const Log &other) const;
     bool operator<(const Log &other) const;
@@ -23,6 +25,26 @@ struct Log{
 };
 
 
-bool Log::operator>(const Log &log1){
+bool Log::operator>(const Log &log1) const {
     return key > log1.key;
+}
+
+bool Log::operator<(const Log &log1) const {
+    return key < log1.key;
+}
+
+bool Log::operator==(const Log&log1) const {
+    return key == log1.key;
+}
+
+bool Log::operator!=(const Log&log1) const {
+    return key != log1.key;
+}
+
+bool Log::operator>=(const Log&log1) const{
+    return key >= log1.key;
+}
+
+bool Log::operator<=(const Log&log1) const {
+    return key <= log1.key;
 }
