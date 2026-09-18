7336f7eb426b551e1f0704571cb2531f7e85fa9a
Isis Krystal Agramón Leal <a00843802@tec.mx>
2026-09-17 22:13:22 -0600

    Evidencia 1. Parte 6

---
+++ Evidences/Evidence1/evidencia.cpp
@@ -0,0 +1,36 @@
+//Isis Krystal Agramón Leal
+//A00843802
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Log.h"
+
+using namespace std;
+
+int main(){
+    ifstream file;
+    file.open("data/log607-1.txt");
+
+    if (!file.is_open()){
+        cout << "Error al abrir el archivo." << endl;
+        return 1;
+    }
+    
+    string line;
+    getline(file, line);
+    stringstream ss(line);
+    string month;
+    int day;
+    string time;
+    string ip;
+    int year;
+    string message;
+    ss >> month >> day >> year >> time >> ip;
+    getline(ss >> ws, message);
+    Log log1(year, month, day, time, ip, message);
+
+    cout << "Key: " << log1.key << endl;
+
+    return 0;
+}
