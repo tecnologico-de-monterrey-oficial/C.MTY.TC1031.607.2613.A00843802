a395dad63e38193e70a0b524ffea378da6410a61
Isis Krystal Agramón Leal <a00843802@tec.mx>
2026-09-18 22:43:01 -0600

    Evidencia 1. Parte 11

--- Evidences/Evidence1/evidencia.cpp
+++ Evidences/Evidence1/evidencia.cpp
@@ -10,49 +10,78 @@
 
 using namespace std;
 
-int main(){
+vector<Log> readFile(string fileName){
+    vector<Log> logs;
     ifstream file;
-    file.open("data/log607-1.txt");
-
+    file.open(fileName);
     if (!file.is_open()){
         cout << "Error al abrir el archivo." << endl;
-        return 1;
+        return logs;
     }
-    
-    vector<Log> logs;
     string line;
-    while(getline(file, line)){
-      stringstream ss(line);  
-
-      string month;
-      int day;
-      string time;
-      string ip;
-      int year;
-      string message;
-      ss >> month >> day >> year >> time >> ip;
-      getline(ss >> ws, message);
-      Log log1(year, month, day, time, ip, message);
-      logs.push_back(log1);
-      
+     while (getline(file, line)){
+        stringstream ss(line);
+        string month;
+        int day;
+        int year;
+        string time;
+        string ip;
+        string message;
+        ss >> month >> day >> year >> time >> ip;
+        getline(ss >> ws, message);
+        Log log1(year, month, day, time, ip, message);
+        logs.push_back(log1);
     }
-    
-cout << "Total de logs: " << logs.size() << endl;
-cout << "Primer log: " << logs[0].key << endl;
-selectionSort(logs);
-
-bool ordenado = true;
-
-for (int i = 1; i < logs.size(); i++) {
-    if (logs[i] < logs[i - 1]) {
-        ordenado = false;
-        break;
-    }
+    return logs;
 }
 
-cout << "Lista ordenada: " << ordenado << endl;
 
-cout << "Primer log ordenado: " << logs[0].key << endl;
-cout << "Ultimo log ordenado: " << logs[logs.size() - 1].key << endl;
+int main(){
+
+    int opcionArchivo;
+    cout << "Selecciona el archivo:" << endl;
+    cout << "Escribe 1 para seleccionar log607-1.txt" << endl;
+    cout << "Escribe 2 para seleccionar log607-2.txt" << endl;
+    cout << "Opcion: ";
+    cin >> opcionArchivo;
+
+    string fileName;
+
+    switch (opcionArchivo){
+    case 1:
+        fileName = "data/log607-1.txt";
+        break;
+
+    case 2:
+        fileName = "data/log607-2.txt";
+        break;
+
+    default:
+        cout << "Opcion invalida." << endl;
+        return 1;
+}
+
+    vector<Log> logs = readFile(fileName);
+    cout << "Archivo seleccionado: " << fileName << endl;
+
+    cout << "Total de logs: " << logs.size() << endl;
+    cout << "Primer log: " << logs[0].key << endl;
+
+    selectionSort(logs);
+
+    bool ordenado = true;
+
+    for (int i = 1; i < logs.size(); i++){
+        if (logs[i] < logs[i - 1]){
+            ordenado = false;
+            break;
+        }
+    }
+
+    cout << "Lista ordenada: " << ordenado << endl;
+
+    cout << "Primer log ordenado: " << logs[0].key << endl;
+    cout << "Ultimo log ordenado: " << logs[logs.size() - 1].key << endl;
+
     return 0;
 }
